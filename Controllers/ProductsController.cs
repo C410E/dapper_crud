de0ef044ee8ed89d38563a167a9053d3e364d05c
caioe <caioevandro0@gmail.com>
2024-02-19 17:37:41 -0300

    Products Create add

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using CrudDapper.Models;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
 namespace CrudDapper.Controllers 
 {
@@ -13,6 +17,34 @@ namespace CrudDapper.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                IDbConnection con;
+
+                try 
+                {
+                    string insercaoQuery = "INSERT INTO products(name)VALUES(@Name)";
+                    con = new NpgsqlConnection(ConnectionString);
+                    con.Open();
+                    con.Execute(insercaoQuery, products);
+                    con.Close();
+                    return RedirectToAction(nameof(Create));
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return View(products);
         }
     }
 }
